27765132b72d91a625b6c4451d593e8aec05d588
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-10-04 12:46:23 +0530

    fix formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import { ThemeProvider } from './context/ThemeContext'
-import { ClerkProvider } from '@clerk/clerk-react'
-import './styles/index.css'
-import './styles/globals.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
+import { ClerkProvider } from '@clerk/clerk-react';
+import './styles/index.css';
+import './styles/globals.css';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -18,5 +18,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         </ThemeProvider>
       </BrowserRouter>
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
